cfc53b4142d2dd6d8f7654f78ddb6bee9a085142
eulegang <eulegang@eulegang.dev>
2021-10-25 17:51:21 -0500

    Removing push_context and it since they are not relavant now

--- src/microunit.h
+++ src/microunit.h
@@ -4,13 +4,10 @@
 typedef void* microunit_ctx;
 
 #define microunit(A) void __microunit_ ## A(microunit_ctx *_ctx)
-#define it(MSG, ...) push_context(_ctx, MSG, ##__VA_ARGS__);
 
 #define assert(A, MSG, ...) if (!(A)) { fail(_ctx, MSG, #__VA_ARGS__); return; }
 #define refute(A, MSG, ...) if ((A)) { fail(_ctx, MSG, #__VA_ARGS__); return; }
 
-
-void push_context(microunit_ctx ctx, char *msg, ...);
 void fail(microunit_ctx ctx, char *msg, ...);
 
 #endif
